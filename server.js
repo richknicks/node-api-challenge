fc67df51cc71a0f24d8778ef7851a8c82f5bfade
richknicks <rlynch@gmail.com>
2020-03-14 12:19:38 -0400

    final commit

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const helmet = require("helmet");
 const actionRouter = require("./data/routers/actionRouter");
 const projectRouter = require("./data/routers/projectRouter");
 const server = express();
+const cors = require('cors');
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 server.use("/api/actions", actionRouter);
 server.use("/api/projects", projectRouter);
